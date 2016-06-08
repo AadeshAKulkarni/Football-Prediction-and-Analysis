2aafcbc48f2b60d4f1f37ce9568c8ab21c20bb2d
Aadesh <aadee.kukani@gmail.com>
2016-06-08 08:23:48 +0530

    Add files via upload

---
+++ Scripts/jquery.common.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+//    $('#ulnavigation li').each(function () {
+//        $(this).find('a').removeClass('active');
+//    });
+//    $('#ulnavigation li').on(function () {
+//        $(this).find('a').addClass('active');
+    //    });
+    $('#myTab').tabCollapse();
+});
